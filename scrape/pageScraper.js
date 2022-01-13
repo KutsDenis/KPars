62c99defac5c550e6014ae6bb2905f050947694f
Den.is.Kuts <97687032+KutsDenis@users.noreply.github.com>
2022-01-14 01:23:06 +0600

    v: 0.1.2
    Refactoring

--- scrape/pageScraper.js
+++ scrape/pageScraper.js
@@ -1,4 +1,12 @@
-const url = 'http://books.toscrape.com';
+const url = 'http://books.toscrape.com'; // http://books.toscrape.com
+
+const selectors = {
+	pageInner: '.page_inner',
+	eval: 'section ol > li',// section ol > li
+	inStock: '.instock.availability > i', //.instock.availability > i
+	textInStock: 'In stock'
+};
+
 const scraperObject = {
 	url: url,
 	async scraper(browser){
@@ -6,11 +14,11 @@ const scraperObject = {
 		console.log('Navigating to ${this.url}...');
 		await page.goto(this.url);
 		// Wait for the required DOM to be rendered
-		await page.waitForSelector('.page_inner');
+		await page.waitForSelector(selectors.pageInner);
 		// Get the link to all the required objects
-		let urls = await page.$$eval('section ol > li', links => {
-			// Make sure the book to be scraped is in stock
-			links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
+		let urls = await page.$$eval(selectors.eval, links => {
+			// Make sure the product to be scraped is in stock
+			links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== 'In stock')
 			// Extract the links from the data
 			links = links.map(el => el.querySelector('h3 > a').href)
 			return links;
